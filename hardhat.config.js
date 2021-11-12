0529ca9b0349400c985a5cadc772d77feb9a5ef8
stfibonacci <87790340+stfibonacci@users.noreply.github.com>
2021-11-11 21:19:29 -0800

    config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,31 +3,15 @@ require("@nomiclabs/hardhat-waffle");
 require('hardhat-gas-reporter');
 
 
-const providerUrl = process.env.MAINNET_PROVIDER_URL;
-const developmentMnemonic = process.env.DEV_ETH_MNEMONIC;
+// const providerUrl = process.env.PROVIDER_URL;
+// const developmentMnemonic = process.env.DEV_MNEMONIC;
 
 //for kovan testnet
 const privateKey = process.env.PRIVATE_KEY;
 
-if (!providerUrl) {
-  console.error('Missing JSON RPC provider URL as environment variable `MAINNET_PROVIDER_URL`\n');
-  process.exit(1);
-}
-
-if (!developmentMnemonic) {
-  console.error('Missing development Ethereum account mnemonic\n');
-  process.exit(1);
-}
-
-if (!privateKey) {
-  console.error('Missing development Ethereum account private key\n');
-  process.exit(1);
-}
-
-
 
 module.exports = {
-  defaultNetwork: "kovan",
+  defaultNetwork: "hardhat",
   paths: {
     artifacts: './src/artifacts',
   },
@@ -36,17 +20,17 @@ module.exports = {
       chainId: 1337,
     },
     // ropsten: {
-    //   url: "https://ropsten.infura.io/v3/projectid",
-    //   accounts: [process.env.a2key]
+    //   url: providerUrl,
+    //   accounts: [`0x${privateKey}`]
     // },
     // rinkeby: {
-    //   url: "https://rinkeby.infura.io/v3/projectid",
-    //   accounts: [process.env.a2key]
+    //   url: providerUrl,
+    //   accounts: [`0x${privateKey}`]
     // },
-    kovan: {
-          url: providerUrl,
-          accounts: [`0x${privateKey}`]
-    },
+    // kovan: {
+    //       url: providerUrl,
+    //       accounts: [`0x${privateKey}`]
+    // },
     
   },
   solidity: "0.8.4",
